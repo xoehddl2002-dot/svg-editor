e4e0fd51b41881ffef780f16e9f48f59d2410695
xoehddl2002 <xoehddl2002@ijaksnc.com>
2026-02-19 15:10:41 +0900

    refactor(설정): 코드 정리 및 프라미스 처리 방식 개선

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,28 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import {worker} from '@/mocks/server.ts'
+import { worker } from '@/mocks/server.ts'
 
+//if(import.meta.env.DEV){
 
-
-if(import.meta.env.DEV){
-
-    await new Promise<void>(resolve =>  worker.start({
-            onUnhandledRequest(request, print) {
-                // Ignore any requests containing "cdn.com" in their URL.
-                if (request.url.includes('src/')||request.url.includes('/')) {
-                    return
-                }
-
-                // Otherwise, print an unhandled request warning.
-                print.warning()
-            },
-        }).then(()=>{
-            resolve()
+await new Promise<void>(resolve =>
+  worker
+    .start({
+      onUnhandledRequest(request, print) {
+        // Ignore any requests containing "cdn.com" in their URL.
+        if (request.url.includes('src/') || request.url.includes('/')) {
+          return
         }
-    ))
 
-}
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
+        // Otherwise, print an unhandled request warning.
+        print.warning()
+      },
+    })
+    .then(() => {
+      resolve()
+    }),
 )
+
+//}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
