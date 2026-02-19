fbf6fa3817b3ba2e068f7d939481ee620abd6dc9
xoehddl2002 <xoehddl2002@ijaksnc.com>
2026-02-19 15:43:02 +0900

    refactor(설정): 코드 주석 처리 방식 변경 및 불필요 조건부 제거

--- src/main.tsx
+++ src/main.tsx
@@ -3,24 +3,24 @@ import App from './App.tsx'
 import './index.css'
 import { worker } from '@/mocks/server.ts'
 
-if (import.meta.env.DEV) {
-  await new Promise<void>(resolve =>
-    worker
-      .start({
-        onUnhandledRequest(request, print) {
-          // Ignore any requests containing "cdn.com" in their URL.
-          if (request.url.includes('src/') || request.url.includes('/')) {
-            return
-          }
+//if (import.meta.env.DEV) {
+await new Promise<void>(resolve =>
+  worker
+    .start({
+      onUnhandledRequest(request, print) {
+        // Ignore any requests containing "cdn.com" in their URL.
+        if (request.url.includes('src/') || request.url.includes('/')) {
+          return
+        }
 
-          // Otherwise, print an unhandled request warning.
-          print.warning()
-        },
-      })
-      .then(() => {
-        resolve()
-      }),
-  )
-}
+        // Otherwise, print an unhandled request warning.
+        print.warning()
+      },
+    })
+    .then(() => {
+      resolve()
+    }),
+)
+//}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
